68fa30e1cb90b1eacdfe2887f6fbe520fe966168
Aleksi Halttunen <halttunen@Aleksis-MacBook-Pro.local>
2020-06-15 16:29:05 +0300

    pre-submission

--- README.md
+++ README.md
@@ -1,10 +1,21 @@
-## pearls
+# PEARLS
 
-A social generative sculpture, to which visitors can contribute by adding or deleting pearls. These changes are then updated (more-or-less) real-time to other visitors on the website. Visitors have the choice to do as they please, and build whatever they want (keeping in mind that it might at any time be deleted by another visitor!).
+A 'social' generative 3D sculpture, which visitors can contribute to by adding or deleting pearls. These changes are then updated (more-or-less) real-time to other visitors on the website. Visitors have the choice to do as they please, and build whatever they want (keeping in mind that it might at any time be destroyed by another visitor!).  
+This was a coursework for a virtual environments class in which I wanted to concentrate on the theme of collaboration. In this project I attempted to build a visually striking front-end design and gain some experience and understanding in back-end websocket and database management.
 
-### technical overview
+## Technical overview
 
-Front-end: [three.js](https://threejs.org/)
-Back-end: [Firebase](https://firebase.google.com/)
+### Front-end
+
+3D world built with [three.js](https://threejs.org/)  
+Controls are utilising three.js OrbitControls 
+Colours are chosen randomly from 6 different color-palettes upon loading the page. 
+
+At the moment the sculpture is only editable on a computer and a browser. You can view the sculpture and follow its progress on mobile, but you cannot contribute to it without a computer.
+
+### Back-end
+
+In order to make this project possible and update real-time, hosting and database management was implemented with [Firebase](https://firebase.google.com/).
+
 
 
--- public/app.js
+++ public/app.js
@@ -5,7 +5,6 @@ var collision= [];
 collision[0] = new THREE.Vector3(0,0,0);
 var cameraPos = new THREE.Vector3();
 var indexSpheres;
-var spacePressed = false;
 var spheresInit = {};
 var index;
 var colors = [];
@@ -26,9 +25,7 @@ var pearlGeometry = new THREE.SphereGeometry( 1, 40, 40 );
 var pearlMaterial = new THREE.MeshPhongMaterial({color:colors[randColor][1], shininess:0});
 var redMaterial = new THREE.MeshPhongMaterial({color:"rgb(200,0,0)", shininess:0, opacity: 0.8, transparent: true});
 var skyBoxMaterial = new THREE.MeshPhongMaterial({color:colors[randColor][0], shininess:0});
-
 const hasPosition = (element) => element.position == collision[0].object.position;
-
 spheres[0] = new THREE.Mesh(pearlGeometry, pearlMaterial);
 var transparentPearl = new THREE.Mesh(pearlGeometry, transparentPearlMaterial);
 var groundMaterial = new THREE.MeshPhongMaterial({color:colors[randColor][2], shininess:0});
@@ -39,86 +36,48 @@ var light = new THREE.DirectionalLight("rgb(255,255,255)");
 var light2 = new THREE.AmbientLight( 0x888888 );
 scene.add( light2 );
 var renderer = new THREE.WebGLRenderer();
-
 controls = new THREE.OrbitControls( camera, renderer.domElement );
-
 controls.enableDamping = true; 
 controls.enableZoom = false;
 controls.dampingFactor = 0.05;
 controls.screenSpacePanning = false;
 controls.maxPolarAngle = (99/100)*Math.PI / 2;
 controls.enableKeys = false;
-
-
 groundMesh.rotation.set(Math.PI/2,0,0);
 groundMesh.material.side=THREE.DoubleSide;
 skyMesh.material.side = THREE.DoubleSide;
 groundMesh.position.set(0,0,0);
-
 camera.position.set( 25, 10, 0 );
-
 light.position.set(2,5,3);
-
-
-
-scene.add(light, groundMesh,skyMesh);
-
-
+scene.add(light, spheres[0],groundMesh,skyMesh);
 renderer.setPixelRatio(window.devicePixelRatio);
-
 renderer.setSize(window.innerWidth, window.innerHeight);
-
 document.body.appendChild(renderer.domElement);
-
 window.addEventListener('resize', onWindowResize, false);  
-    
 window.addEventListener('keyup', function (e) {
     keyPress = e.keyCode;
     });
 
-
 firebase.database().ref('sculptureData').once('value').then(function(snapshot) {
-    console.log(snapshot.child("obj").numChildren());
-    for (var i=0; i<snapshot.child("obj").numChildren(); i++){
+    console.log(snapshot.numChildren());
+    for (var i=1; i<snapshot.numChildren(); i++){
         console.log("creating sphere "+i);
-        spheres.push( new THREE.Mesh(pearlGeometry, pearlMaterial));
-        spheres[i].position.set(snapshot.child("obj").child(i).child("x").val(), snapshot.child("obj").child(i).child("y").val(), snapshot.child("obj").child(i).child("z").val());
+        spheres[i]= new THREE.Mesh(pearlGeometry, pearlMaterial);
+        spheres[i].position.set(snapshot.child(i).child("x").val(), snapshot.child(i).child("y").val(), snapshot.child(i).child("z").val());
         scene.add(spheres[i]);
-      
-        }
-    });
-
-
+    }
+});
 
 function writeSculptureData(obj) {
-        firebase.database().ref('sculptureData').set({
-        obj});
-        }
-
-        function indexSender(index) {
-            firebase.database().ref('index').set(
-            index);
-            }        
-/*
-function writeUserData(x,y,z) {
-    firebase.database().ref('sphereAdded').set({
-        X: x,
-        Y: y,
-        Z: z
-        });
-    }
-function writeUserDataDelete(index) {
-    firebase.database().ref('sphereDeleted').set({
-        INDEX: index
-        });
+    firebase.database().ref('sculptureData').set(obj);
 }
-*/
-
+function indexSender(index) {
+    firebase.database().ref('index').set(index);
+}        
 firebase.database().ref('index').on('value', function(snapshot) {
     index = snapshot.val();
-    });
-
-firebase.database().ref('sculptureData/obj').on('value', function(snapshot) {
+});
+firebase.database().ref('sculptureData').on('value', function(snapshot) {
     console.log("reading sculpture data");
     if (spheres.length < snapshot.numChildren()) {
         spheres.push(new THREE.Mesh(pearlGeometry, pearlMaterial));
@@ -130,53 +89,19 @@ firebase.database().ref('sculptureData/obj').on('value', function(snapshot) {
     if (spheres.length > snapshot.numChildren()) {
         scene.remove(spheres[index]); 
         spheres.splice(index,1);
-        
-        
         }
-    });
-/*
-firebase.database().ref('sphereAdded/X').on('value', function(snapshot) {
-    console.log("X read");
-    spheres.push(new THREE.Mesh(pearlGeometry, pearlMaterial));
-    spheres[spheres.length-1].position.x=snapshot.val();
-    });
-firebase.database().ref('sphereAdded/Y').on('value', function(snapshot) {
-
-    console.log("Y read");
-    spheres[spheres.length-1].position.y=snapshot.val();
 });
-firebase.database().ref('sphereAdded/Z').on('value', function(snapshot) {
-    
-    console.log("Z read");
-    if (spacePressed == true){
-    spheres[spheres.length-1].position.z=snapshot.val();
-    scene.add(spheres[spheres.length-1]);   
-    }
-});
-
-
-firebase.database().ref('sphereDeleted/INDEX').on('value', function(snapshot) {
-    scene.remove(spheres[snapshot.val()]); 
-    spheres.splice(snapshot.val(),1);
-});
-
-    */
-
 
 function draw() {
-
-    
     camera.getWorldPosition(cameraPos);
     var direction = cameraPos.clone().negate().normalize();
     var rayCaster = new THREE.Raycaster(cameraPos, direction); 
     collision = rayCaster.intersectObjects(spheres); 
-//adding sheres
+    //adding sheres
     if (toggle%2 == 0) {
         scene.add(transparentPearl);
         transparentPearl.position.set(collision[0].point.x, collision[0].point.y, collision[0].point.z);
-        if (keyPress == 32) {
-            spacePressed = true;
-            //writeUserData(transparentPearl.position.x, transparentPearl.position.y, transparentPearl.position.z);
+        if (keyPress == 32 && collision[0].point.length() < 17) {
             spheres.push(new THREE.Mesh(pearlGeometry, pearlMaterial));
             spheres[spheres.length-1].position.x=transparentPearl.position.x;
             spheres[spheres.length-1].position.y=transparentPearl.position.y;
@@ -184,18 +109,18 @@ function draw() {
             scene.add(spheres[spheres.length-1]);
             console.log(spheresInit);
             keyPress = 0;
+            spheresInit = [];
             for (var i = 0; i < spheres.length; i++) {
                 spheresInit[i] = {
                     x: spheres[i].position.x,
                     y: spheres[i].position.y,
                     z: spheres[i].position.z,
-              };
+                };
             }
             writeSculptureData(spheresInit);
-        
         }
     }
-//toggle adding/deleting spheres
+    //toggle adding/deleting spheres
     if (keyPress == 68) {
         toggle += 1;
         if (toggle%2 == 0) {
@@ -205,7 +130,7 @@ function draw() {
         }
         keyPress = 0;
     } 
-//deleting spheres
+    //deleting spheres
     if (toggle%2 == 1) {
         scene.remove(transparentPearl);
             collision[0].object.material=redMaterial;
@@ -215,44 +140,38 @@ function draw() {
             }
         } 
         if (keyPress == 32) {
-            spacePressed = true;
             var sphereIndex = spheres.findIndex(hasPosition);
+            console.log(sphereIndex);
             indexSender(sphereIndex);
             if (sphereIndex != 0) {
                 scene.remove(spheres[sphereIndex]); 
                 spheres.splice(sphereIndex,1);
-                
-            //scene.remove(spheres[sphereIndex]); CALL ON LISTEN
-            //spheres.splice(sphereIndex,1);
-            //writeUserDataDelete(sphereIndex);
+                console.log(spheres.length);
+
             keyPress = 0;
+            spheresInit=[]
             for (var i = 0; i < spheres.length; i++) {
                 spheresInit[i] = {
                     x: spheres[i].position.x,
                     y: spheres[i].position.y,
                     z: spheres[i].position.z,
-              };
+                };
             }
-            writeSculptureData(spheresInit);
-            
+            writeSculptureData(spheresInit);   
             }
-        }
-        
+        }   
     }
-
     controls.update();
     renderer.render(scene, camera);
     requestAnimationFrame(draw);
-
 }
 
-     
 function onWindowResize() {
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
 	renderer.setSize(window.innerWidth, window.innerHeight);
 }    
 
-    draw();   // the variable is defined
+draw();  
 
 
